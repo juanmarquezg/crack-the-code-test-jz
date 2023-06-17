f978f853c93e0987081e79990466aeb75d88dcea
jcmarquezg <jcmarquezgalvis@gmail.com>
2023-06-16 19:55:17 -0500

    Github actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://juanmarquezg.github.io/crack-the-code-test-jz/',
   plugins: [react()],
 })
